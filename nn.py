a27ab5c9bf9df784507308f9cfd3037c03b77d4d
Harshit Gupta <35541983+harshit0921@users.noreply.github.com>
2019-04-24 23:19:35 -0400

    Update nn.py

--- nn.py
+++ nn.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 20 00:50:31 2019
-
-@author: shivamodeka
-"""
-
 from merge_data import create_breast_data, get_data_skin
 import torch
 import torch.nn.functional as F
